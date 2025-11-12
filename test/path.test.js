5484c6353da86383a5f35ff5edc380447e530a0e
Conrad Buck <conartist6@gmail.com>
2025-11-12 12:07:53 -0700

    cleanup and fixes

--- test/path.test.js
+++ test/path.test.js
@@ -19,7 +19,7 @@ import { expect } from 'expect';
 
 let tags = [
   buildOpenNodeTag(fragmentFlags),
-  buildReferenceTag('.'),
+  buildReferenceTag('..'),
   buildBindingTag(),
   buildOpenNodeTag(nodeFlags, 'Node'),
   buildReferenceTag(null, 'inner'),
@@ -41,7 +41,7 @@ describe('Path', () => {
       let doc = treeFromStream([
         buildDoctypeTag(),
         buildOpenNodeTag(fragmentFlags),
-        buildReferenceTag('.'),
+        buildReferenceTag('..'),
         buildOpenNodeTag(nodeFlags, 'Foo'),
         buildReferenceTag(null, 'bar'),
         buildOpenNodeTag(nodeFlags, 'Bar'),
